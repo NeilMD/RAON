acfa2fe90ea8b1641feda997a9d3970a0f663210
neilmd <neil.capistrano2007@gmail.com>
2025-04-15 02:48:12 -0600

    feat:added project page

---
+++ js/projects.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", () => {
+  const heroImg = document.getElementById("hero-img");
+  const imgCon = document.getElementById("img-con");
+  const contactSection = document.getElementById("video");
+
+  // Get position of img-con relative to viewport
+  const bounds = imgCon.getBoundingClientRect();
+
+  // Convert to position relative to #contact section
+  const contactBounds = contactSection.getBoundingClientRect();
+
+  const fromX = bounds.left - contactBounds.left;
+  const fromY = bounds.top - contactBounds.top;
+
+  const fromWidth = bounds.width;
+  const fromHeight = bounds.height;
+
+  //   // Set transform origin to top left
+  //   gsap.set(heroImg, {
+  //     transformOrigin: "top left",
+  //   });
+
+  // Animate from position of #img-con to full section
+  gsap.from(heroImg, {
+    scrollTrigger: {
+      trigger: "#video",
+      start: "top bottom",
+      end: "top top",
+      scrub: true,
+    },
+    x: fromX,
+    y: fromY,
+    width: fromWidth,
+    height: fromHeight,
+    ease: "none",
+  });
+});
